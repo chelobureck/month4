00360b6908b4ca5bf00ed2b68697f24126eb092a
chelobureck <adylbekoveldiyar@gmail.com>
2025-11-25 20:43:57 +0600

    lesson 1

--- blog/urls.py
+++ blog/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from posts.views import test_view
+from posts.views import test_view, html_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("test/", test_view)
+    path("test/", test_view),
+    path("base/", html_view)
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from random import randint
 
 def test_view(request):
-    
-    return HttpResponse("This is a test view")
+
+    return HttpResponse(f"This is a test view {randint(1, 1000)}")
+
+def html_view(request):
+    return render(request, "base.html")
 
 # Create your views here.
