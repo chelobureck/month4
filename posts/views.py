a0cddd31ae712d79a13a749807e74aec067f83ae
chelobureck <adylbekoveldiyar@gmail.com>
2025-11-25 20:54:07 +0600

    lesson 1

--- posts/views.py
+++ posts/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse
 from random import randint
 
 def test_view(request):
-
     return HttpResponse(f"This is a test view {randint(1, 1000)}")
 
 def html_view(request):
