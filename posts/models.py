8dd34e5851251aa293562180d6a1719ebfa713cc
chelobureck <adylbekoveldiyar@gmail.com>
2025-12-05 14:53:14 +0600

    hw3

--- posts/models.py
+++ posts/models.py
@@ -6,13 +6,29 @@ posts = Post.objects.get(id=1) только универсальное знач�
 posts = Post.objects.filter()
 """
 
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+    
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
 class Post(models.Model):
+    image = models.ImageField(null=True, blank=True)
     name = models.CharField(max_length=255)
     content = models.CharField(max_length=1000, null=True, blank=True)
     rate = models.IntegerField(default=0, null=True)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
 
+    # связи
+    tags = models.ManyToManyField(Tag, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self) -> str:
         return f"{self.name} - {self.content}"
